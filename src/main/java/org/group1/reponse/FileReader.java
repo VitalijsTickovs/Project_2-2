c247a53cf7aff49bd7525731654f995249f8c564
tombakker <bakker02tom@gmail.com>
2023-02-28 14:36:15 +0100

    Added new rule and test cases in filereader

--- src/main/java/org/group1/reponse/FileReader.java
+++ src/main/java/org/group1/reponse/FileReader.java
@@ -17,6 +17,14 @@ public class FileReader {
         generateSkills();
     }
 
+    public String getAnswer(String question) throws Exception {
+        for (Skill skill : skills) {
+            if(skill.isMatch(question))
+                return skill.getAnswer(question);
+        }
+        return null;
+    }
+
     public void generateSkills() throws IOException {
         List<String> text = service.readAll();
 
@@ -31,8 +39,19 @@ public class FileReader {
 
     public static void main(String[] args) throws Exception {
         FileReader file = new FileReader();
-        System.out.println(file.skills.get(0).getAnswer("What lecture do we have on monday at 11?"));
-        System.out.println(file.skills.get(0).getAnswer("On monday at 9 what lecture do we have?"));
+        System.out.println(file.getAnswer("What lecture do we have on monday at 11?"));
+        System.out.println(file.getAnswer("On monday at 9 what lecture do we have?"));
+        System.out.println(file.getAnswer("What lecture do we have on monday at 9?"));
+        System.out.println(file.getAnswer("What will the weather be like on tuesday?"));
+        System.out.println("-------------------------------------------------");
+        //some test cases from monday to sunday with different formulation
+        System.out.println(file.getAnswer("What is the weather on monday?"));
+        System.out.println(file.getAnswer("On tuesday what is the weather like?"));
+        System.out.println(file.getAnswer("On wednesday what is the weather like?"));
+        System.out.println(file.getAnswer("How is the weather on thursday?"));
+        System.out.println(file.getAnswer("What is the weather on friday?"));
+        System.out.println(file.getAnswer("What is the weather on saturday?"));
+        System.out.println(file.getAnswer("What is the weather on sunday?"));
 
     }
 }
