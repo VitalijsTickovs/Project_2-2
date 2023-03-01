7003a35015c56620153dca9de55f977cb98d134a
piedeboer96 <piedeboer96@gmail.com>
2023-03-01 12:58:47 +0100

    added jacard similarity, based on set theory

--- src/main/java/org/group1/response/distances/JaccardSimilarity.java
+++ src/main/java/org/group1/response/distances/JaccardSimilarity.java
@@ -2,37 +2,49 @@ package org.group1.response.distances;
 
 import java.util.HashSet;
 
+
 public class JaccardSimilarity {
     public static double calculate(String s1, String s2) {
+
+
+
+        // create two hashSets to allow for set operations
         HashSet<String> set1 = new HashSet<>();
         HashSet<String> set2 = new HashSet<>();
 
-        // Split strings into words
+        // split strings into words
         String[] words1 = s1.split("\\s+");
         String[] words2 = s2.split("\\s+");
 
-        // Add words to sets
+        // put words into lower case and add words to sets
         for (String word : words1) {
+            word.toLowerCase();
             set1.add(word);
         }
         for (String word : words2) {
+            word.toLowerCase();
             set2.add(word);
         }
 
-        // Calculate Jaccard similarity
+        // create the union
         HashSet<String> union = new HashSet<>(set1);
         union.addAll(set2);
 
+        // create the intersection set
         HashSet<String> intersection = new HashSet<>(set1);
         intersection.retainAll(set2);
 
+        // calculate similarity between the two original strings
+        // based on the division of the cardinality of union set and intersection set
         double similarity = (double) intersection.size() / (double) union.size();
+
         return similarity;
     }
 
     public static void main(String[] args) {
-        String s1 = "the quick brown fox jumps over the lazy dog";
-        String s2 = "the lazy dog jumps over the quick brown fox";
+
+        String s1 = "Tom likes to eat cake";
+        String s2 = "cake and cats for breakfast";
 
         double similarity = JaccardSimilarity.calculate(s1, s2);
         System.out.println("Jaccard similarity = " + similarity);
