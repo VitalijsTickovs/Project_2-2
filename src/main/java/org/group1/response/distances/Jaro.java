c36dda2bfd093bae3c27e31cdbb42019c5b30f0b
VitalijsTickovs <v.tickovs@student.maastrichtuniversity.nl>
2023-03-01 13:23:58 +0100

    Jaro Distance commented

--- src/main/java/org/group1/response/distances/Jaro.java
+++ src/main/java/org/group1/response/distances/Jaro.java
@@ -1,47 +1,45 @@
 package org.group1.response.distances;
 
 public class Jaro {
-    public static double jaro_distance(String s1, String s2){
+    public static double jaroDistance(String a, String b){
         // If the Strings are equal
-        if (s1.equals(s2))
+        if (a.equals(b))
             return 1.0;
 
         // Length of two Strings
-        int len1 = s1.length(),
-                len2 = s2.length();
+        int aLength = a.length();
+        int bLength = b.length();
 
         // Maximum distance upto which matching
         // is allowed
-        int max_dist = (int) (Math.floor(Math.max(len1, len2) / 2) - 1);
+        int maxDist = (int) (Math.floor(Math.max(aLength, bLength) / 2) - 1);
 
         // Count of matches
         int match = 0;
 
         // Hash for matches
-        int hash_s1[] = new int[s1.length()];
-        int hash_s2[] = new int[s2.length()];
+        int aHash[] = new int[a.length()];
+        int bHash[] = new int[b.length()];
 
         // Traverse through the first String
-        for (int i = 0; i < len1; i++)
-        {
+        for (int i = 0; i < aLength; i++) {
 
             // Check if there is any matches
-            for (int j = Math.max(0, i - max_dist);
-                 j < Math.min(len2, i + max_dist + 1); j++)
+            for (int j = Math.max(0, i - maxDist);
+                 j < Math.min(bLength, i + maxDist + 1); j++) {
 
                 // If there is a match
-                if (s1.charAt(i) == s2.charAt(j) && hash_s2[j] == 0)
-                {
-                    hash_s1[i] = 1;
-                    hash_s2[j] = 1;
+                if (a.charAt(i) == b.charAt(j) && bHash[j] == 0) {
+                    aHash[i] = 1;
+                    bHash[j] = 1;
                     match++;
                     break;
                 }
+            }
         }
 
         // If there is no match
-        if (match == 0)
-            return 0.0;
+        if (match == 0) return 0.0;
 
         // Number of transpositions
         double t = 0;
@@ -52,34 +50,30 @@ public class Jaro {
         // where two characters match but
         // there is a third matched character
         // in between the indices
-        for (int i = 0; i < len1; i++)
-            if (hash_s1[i] == 1)
-            {
-
-                // Find the next matched character
-                // in second String
-                while (hash_s2[point] == 0)
+        for (int i = 0; i < aLength; i++) {
+            if (aHash[i] == 1) {
+                // Find the next matched character in second string
+                while (bHash[point] == 0)
                     point++;
 
-                if (s1.charAt(i) != s2.charAt(point++) )
+                if (a.charAt(i) != b.charAt(point++))
                     t++;
             }
+        }
 
         t /= 2;
 
         // Return the Jaro Similarity
-        return (((double)match) / ((double)len1)
-                + ((double)match) / ((double)len2)
-                + ((double)match - t) / ((double)match))
-                / 3.0;
+        return (    ((double)match) / ((double)aLength)+
+                    ((double)match) / ((double)bLength)+
+                    ((double)match - t) / ((double)match)) /3; //Divide by 3 to normalize it
     }
 
-    // Driver code
     public static void main(String[] args)
     {
         String s1 = "hwat lecrure thre monaday", s2 = "Which lectures are there on Monday";
 
         // Print jaro Similarity of two Strings
-        System.out.print(jaro_distance(s1, s2) +"\n");
+        System.out.print(jaroDistance(s1, s2) +"\n");
     }
 }
