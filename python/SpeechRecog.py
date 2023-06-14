cb311d8152a82729e2eae38006df8f1046431a02
PodevynLoris <115578089+PodevynLoris@users.noreply.github.com>
2023-06-14 13:14:49 +0200

    AudioRecording
    Ability to record and transcribe in real time

--- python/SpeechRecog.py
+++ python/SpeechRecog.py
@@ -2,25 +2,17 @@ import torch
 import librosa
 from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor
 
-MODEL_ID = "jonatasgrosman/wav2vec2-large-xlsr-53-english"
+class SpeechRecognizer:
+    def __init__(self, model_id):
+        self.processor = Wav2Vec2Processor.from_pretrained(model_id)
+        self.model = Wav2Vec2ForCTC.from_pretrained(model_id)
 
-processor = Wav2Vec2Processor.from_pretrained(MODEL_ID)
-model = Wav2Vec2ForCTC.from_pretrained(MODEL_ID)
+    def transcribe(self, audio_file):
+        speech_array, _ = librosa.load(audio_file, sr=16_000)
+        inputs = self.processor(speech_array, sampling_rate=16_000, return_tensors="pt", padding=True)
 
-def speech_file_to_array_fn(file_path):
-    speech_array, sampling_rate = librosa.load(file_path, sr=16_000)
-    return speech_array
+        with torch.no_grad():
+            logits = self.model(inputs.input_values, attention_mask=inputs.attention_mask).logits
 
-# The audio file has to be in WAV format and 16.000 Hz
-audio_file = "/Users/lorispodevyn/Documents/JavaBook/vitaly16hz.wav"
-speech_array = speech_file_to_array_fn(audio_file)
-
-inputs = processor(speech_array, sampling_rate=16_000, return_tensors="pt", padding=True)
-
-with torch.no_grad():
-    logits = model(inputs.input_values, attention_mask=inputs.attention_mask).logits
-
-predicted_ids = torch.argmax(logits, dim=-1)
-predicted_sentence = processor.decode(predicted_ids[0])
-
-print("Prediction:", predicted_sentence)
+        predicted_ids = torch.argmax(logits, dim=-1)
+        return self.processor.decode(predicted_ids[0])
